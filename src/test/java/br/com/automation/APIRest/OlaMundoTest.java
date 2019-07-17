01a1599ce7ef55c0efb61f3b3c74d348acfc14b6
rodrigo.deus <rodrigo.deus@inmetrics.com.br>
2019-07-17 00:05:07 -0300

    Classe de teste

--- src/test/java/br/com/automation/APIRest/OlaMundoTest.java
+++ src/test/java/br/com/automation/APIRest/OlaMundoTest.java
@@ -1,21 +1,27 @@
 package br.com.automation.APIRest;
 
+import org.junit.Test;
+
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
 import io.restassured.response.Validatable;
 import io.restassured.response.ValidatableResponse;
 
-public class OlaMundo {
+public class OlaMundoTest {
 	
-	public static void main(String[] args) {
+
+	@Test
+	public void OlaMundo () {
+		
 		Response response = RestAssured.request(Method.GET, "https://restapi.wcaquino.me/ola");
 		System.out.println(response.getBody().asString().equals("Ola Mundo!"));
 		System.out.println(response.statusCode() == 200);
 		
 		ValidatableResponse validacao = response.then();
-				validacao.statusCode(200);		
-	} 
+				validacao.statusCode(200);	
+		
+	}
  
 }
  
